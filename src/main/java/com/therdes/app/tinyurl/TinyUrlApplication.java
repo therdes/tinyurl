627fbc7017eeffb53726e4bfbef5b247990a0204
therdes <therdesok@gmail.com>
2019-08-13 10:19:14 +0800

    增加MySQL连接依赖，增加运行配置和测试用配置

--- src/main/java/com/therdes/app/tinyurl/TinyUrlApplication.java
+++ src/main/java/com/therdes/app/tinyurl/TinyUrlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinyurlApplication {
+public class TinyUrlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TinyurlApplication.class, args);
+		SpringApplication.run(TinyUrlApplication.class, args);
 	}
 
 }
--- src/test/java/com/therdes/app/tinyurl/TinyUrlApplicationTests.java
+++ src/test/java/com/therdes/app/tinyurl/TinyUrlApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TinyurlApplicationTests {
+public class TinyUrlApplicationTests {
 
 	@Test
 	public void contextLoads() {
