f1c3a8d675a2ba496ed77d9747100ce119b6ee35
therdes <therdesok@gmail.com>
2019-08-13 10:04:07 +0800

    初始化提交

---
+++ src/test/java/com/therdes/app/tinyurl/TinyurlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.therdes.app.tinyurl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyurlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
